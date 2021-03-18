fdb8160969496607375edf5072f1a322d2086e72
uncia <melirandeu@gmail.com>
2021-03-19 08:18:53 +0900

    [update]デザインを大幅に更新

---
+++ appserver.py
@@ -0,0 +1,14 @@
+# appserver.py
+from backend.application import create_app
+from flask import render_template
+app = create_app()
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template("index.html")
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
